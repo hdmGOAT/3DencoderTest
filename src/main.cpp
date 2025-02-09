5fae8884baee8ddf4ee60a2a233168fbd19bf8e1
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-02-09 13:13:08 +0800

    sakit sa ulo

--- src/main.cpp
+++ src/main.cpp
@@ -7,4 +7,18 @@ using namespace std;
 int main(){
     Mat image = Mat::zeros(300,300,CV_8UC3);
     circle(image, Point(150,150), 100, Scalar(0,255,0),-1);
+    
+    rectangle(image, Point(100, 100), Point(400, 400), Scalar(255, 0, 0), 2);
+
+    line(image, Point(100, 400), Point(400, 100), Scalar(0, 0, 255), 3);
+
+    putText(image, "OpenCV Test", Point(120, 480), FONT_HERSHEY_SIMPLEX, 1, Scalar(255, 255, 255), 2);
+
+    imshow("Test Window", image);
+
+    imwrite("output_test_image.jpg", image);
+
+    waitKey(0);
+
+    return 0;
 }
