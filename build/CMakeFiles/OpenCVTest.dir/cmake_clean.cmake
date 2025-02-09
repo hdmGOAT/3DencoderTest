83773ff6a0c169c94617394688b859e8e9162f6d
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-02-09 13:44:24 +0800

    okay it works in vs

---
+++ build/CMakeFiles/OpenCVTest.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenCVTest.dir/src/main.cpp.obj"
+  "CMakeFiles/OpenCVTest.dir/src/main.cpp.obj.d"
+  "OpenCVTest.exe"
+  "OpenCVTest.exe.manifest"
+  "OpenCVTest.pdb"
+  "libOpenCVTest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenCVTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
