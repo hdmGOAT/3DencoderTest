99fa005a38a845c9a6901c05ebed1e64bd2f2f29
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-02-09 13:58:47 +0800

    test encoder

---
+++ src/functions/encoder.cpp
@@ -0,0 +1,31 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <bitset>
+#include <string>
+
+using namespace cv;
+using namespace std;
+
+void encodeTextToImage(const string& text, const string& outputImagePath) {
+
+    string binaryData;
+    for (char c : text) {
+        binaryData += bitset<8>(c).to_string(); 
+    }
+
+    int side = ceil(sqrt(binaryData.length())); 
+    Mat image = Mat::zeros(side, side, CV_8UC1);
+
+    int bitIndex = 0;
+    for (int i = 0; i < image.rows; ++i) {
+        for (int j = 0; j < image.cols; ++j) {
+            if (bitIndex < binaryData.length()) {
+                image.at<uchar>(i, j) = (binaryData[bitIndex] == '1') ? 255 : 0; 
+                bitIndex++;
+            }
+        }
+    }
+
+    imwrite(outputImagePath, image);
+    cout << "Text encoded into image: " << outputImagePath << endl;
+}
